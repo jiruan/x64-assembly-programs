6b63fcd911c71d0e715a4b2f8e3f9072cf0df100
jiruan <jiruan@umich.edu>
2020-09-23 22:11:05 -0500

    73

--- ex73.asm
+++ ex73.asm
@@ -1,11 +1,27 @@
 global _start
 
+        segment .data
+a       dq      0xFFFFFFFFFFFFFFFF
+
         segment .text
 ;rdi
 bytexors:
     mov rdx, rdi
-    mov rax, 0
-    mov al, dl
-    
+    xor rax, rax
 
-_start:   
+    mov al, dl 
+    xor al, dh
+
+    ror rdx, 16
+    xor al, dl
+    xor al, dh      
+
+    ret
+
+_start:
+    mov rdi, [a]
+    call bytexors 
+
+    mov rdi, rax
+    mov rax, 60
+    syscall
